cc7a4666e163ab64bdb80843149a41a3ed507ed3
edcalderon <edwardca12@hotmail.com>
2024-10-10 16:06:45 -0500

    working dir

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -11,12 +11,12 @@ const config: Config = {
   url: 'https://alternun-development.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/alternun/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'alternun-development', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'alternun', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
