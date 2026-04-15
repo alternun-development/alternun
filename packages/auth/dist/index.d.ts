8db9657a8b6c4d5b71adc32ec219afd1ceb449a2
edcalderon <edwardca12@hotmail.com>
2026-04-15 06:03:36 -0500

    fix(docs,mobile,auth): CHANGELOG, changelogData

--- packages/auth/dist/index.d.ts
+++ packages/auth/dist/index.d.ts
@@ -7,18 +7,10 @@ export type {
   SignInOptions,
   User,
 } from '@edcalderon/auth';
-export { AppAuthProvider, useAuth, type AppAuthProviderProps } from './mobile/AppAuthProvider';
-export { AlternunAuthFacade } from './facade/AlternunAuthFacade';
-export { createAuthFacade } from './facade/createAuthFacade';
 export {
   AuthentikIssuerProvider,
   type AuthentikIssuerProviderOptions,
 } from './providers/authentik/AuthentikIssuerProvider';
-export {
-  BetterAuthExecutionProvider,
-  type BetterAuthExecutionProviderOptions,
-  type BetterAuthClientLike,
-} from './providers/better-auth/BetterAuthExecutionProvider';
 export {
   SupabaseExecutionProvider,
   type SupabaseExecutionProviderOptions,
